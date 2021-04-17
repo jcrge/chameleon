c3d761450b156413a78a0a134ecc2ee7b7b3045d
Jorge <jcal.d.as.1.9.99+vivasgithub@gmail.com>
2021-04-17 11:21:24 +0200

    Añadir el componente AutoPlayer, todavía no funcional.
    Actualmente se expande para ocupar todo el espacio del padre
    horizontalmente, independientemente de si su valor para
    android:layout_width es match_parent o wrap_content. Sin embargo, sí
    respeta el espacio vertical.

---
+++ Chameleon/AudioPlayer.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chameleon
+{
+    public class AudioPlayer : LinearLayout
+    {
+        TextView PositionText;
+        Button PlayPauseButton;
+        Button RewindButton;
+        Button FastForwardButton;
+        CheckBox LoopCheckBox;
+
+        public AudioPlayer(Context context) : base(context)
+        {
+            Initialize();
+        }
+
+        public AudioPlayer(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public AudioPlayer(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            LayoutInflater inflater = (LayoutInflater)Context.GetSystemService(Context.LayoutInflaterService);
+            View view = inflater.Inflate(Resource.Layout.audio_player, null, true);
+            view.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
+            AddView(view);
+
+            PositionText = view.FindViewById<TextView>(Resource.Id.position_text);
+            PlayPauseButton = view.FindViewById<Button>(Resource.Id.play_pause_button);
+            RewindButton = view.FindViewById<Button>(Resource.Id.rewind_button);
+            FastForwardButton = view.FindViewById<Button>(Resource.Id.fastforward_button);
+            LoopCheckBox = view.FindViewById<CheckBox>(Resource.Id.loop_checkbox);
+        }
+    }
+}
